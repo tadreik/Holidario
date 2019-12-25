8de5d07a54beef9309be10c7361f1665461c06b3
Tadreik <tadreik@gmail.com>
2019-12-25 18:14:19 -0500

    new AR

--- Holidario/ViewController.swift
+++ Holidario/ViewController.swift
@@ -16,10 +16,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // Load the "Enter" scene from the "Holidario" Reality File
+        let enterAnchor = try! Holidario.loadEnter()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(enterAnchor)
     }
 }
