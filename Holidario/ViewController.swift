72169fee78f92aebe213ad4c3bd66adea5fa2b4a
Tadreik <tadreik@gmail.com>
2019-12-25 14:42:11 -0500

    Initial Commit

---
+++ Holidario/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Holidario
+//
+//  Created by Tadreik Campbell on 12/25/19.
+//  Copyright © 2019 Tadreik Campbell. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
