c574a6ccc87e9339b4281c12ec585f84eb89560c
Tadreik Campbell <tadreik@gmail.com>
2021-01-06 12:38:23 -0500

    u

--- Holidario/AppDelegate.swift
+++ Holidario/AppDelegate.swift
@@ -17,11 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let landingPage = LandingPage()
 
         // Use a UIHostingController as window root view controller.
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: contentView)
+        window.rootViewController = UIHostingController(rootView: landingPage)
         self.window = window
         window.makeKeyAndVisible()
         return true
